a1affad26fbcd5a14be5db9c71d8e0a250e0b9e3
Ubuntu <tommaso.desoldaee@gmail.com>
2020-11-09 10:37:08 +0000

    allargacoppie ed ins,elim array

---
+++ allargacoppie.c
@@ -0,0 +1,50 @@
+/* 
+
+Descrizione programma: void allargacoppie (int A[], int n), per ogni coppia di elementi adiacenti, 
+se il primo è minore del secondo, sottrae 1 al primo e aggiunge 1 al secondo
+
+Dati in ingresso: array di int, e la sua dimensione
+
+Dati in uscita: l'array modificato opportunamente
+
+Precondizione: l'array non deve essere vuoto, e di int
+
+Postcondizone: deve avere la stessa dimensione dell'array in ingresso, e modificato come traccia
+
+Pseudocodice:
+
+*/
+
+#include <stdio.h>
+#define N 100
+
+void allargacoppie(int A[], int n){
+    for (int i = 0; i < n - 1; i++) {  //i arriva a n - 1 poichè l'ultimo elemento non ha un compagno, e non va controllato
+        if (A[i] < A[i + 1]){
+            A[i] -= 1;
+            A[i + 1] += 1;
+        }     
+
+    }
+    return;
+}
+void stampa_array_int(int arr[], int dim){
+    for (int p = 0; p < dim; p++){
+        printf("Numero in posizione %d:  %d\n", p, arr[p]);
+    }
+    return;
+}
+
+int main (void){
+
+    int vett[N] = {33, 21, -7, 9, -100};
+    int vett_dim = 5;
+
+    allargacoppie(vett, vett_dim);
+
+    stampa_array_int(vett, vett_dim);
+
+
+    return 0;
+}
+
---
+++ inserisci-elimina-el-array.c
@@ -0,0 +1,75 @@
+/* 
+
+Descrizione programma: aggiungiel(a[], n, el, index) aggiunge l'elemento el nell'opportuna posizione,
+rimuoviel(a[], n, index) rimuove l'elemento [index] dall'array
+
+Dati in ingresso: per aggiungi, array, dimensione, elemento e l'indice dove inserirlo
+    per rimuovi array, dimensione e l'indice da eliminare
+
+Dati in uscita: l'array opportunamente modificato, inoltre ritorna la nuova dimensione
+
+Precondizione: l'indice dove inserire o rimuovere l'elemento deve essere valido
+
+Postcondizone: l'array deve essere opportunamente modificato, come la dimensione
+
+Pseudocodice:
+
+*/
+
+#include <stdio.h>
+#define N 100
+
+int aggiungiel(int A[], int dim, int el, int index){
+
+    for(int i = dim + 1; i >= index; i--) //spostiamo ogni elemento dopo index a destra di una casella
+        A[i] = A[i - 1];
+
+    A[index] = el; //e mettiamo el in index
+    
+
+    return dim + 1; //ritorniamo la dimensione incrementata
+}
+
+int rimuoviel(int A[], int dim, int index){
+
+    for(int i = index; i < dim; i++) // spostiamo ogni elemento a sinistra di una casella
+        A[i] = A[i + 1]; // sovrascrivendo l'elemento in posizione index
+    
+    return dim - 1; //ritorniamo la dimensione decrementata
+}
+
+void riempi_array_int(int A[], int dim){
+
+    for (int i = 0; i < dim; i++){
+        printf("\nPer favore, inserisci l'elemento %d:  ", i);
+        scanf("%d", &A[i]);
+    }
+}
+
+void stampa_array_int(int arr[], int dim){
+    for (int p = 0; p < dim; p++){
+        printf("Numero in posizione %d:  %d\n", p, arr[p]);
+    }
+    return;
+}
+
+int main (void){
+
+    int vett1[N] = {23, 11, 55, 65, 12};
+    int dim1 = 5;
+
+    stampa_array_int(vett1, dim1);
+
+    dim1 = rimuoviel(vett1, dim1, 2);
+
+    stampa_array_int(vett1, dim1);
+
+    dim1 = aggiungiel(vett1, dim1, 55, 2);
+
+    stampa_array_int(vett1, dim1);
+
+
+
+    return 0;
+}
+
