07ec6c974dc1ac43d4520af8b19f3fa13a122712
mercury501 <tommaso.desoldaee@gmail.com>
2021-02-15 11:52:07 +0100

    Update binfiles.c

--- files/binfiles.c
+++ files/binfiles.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 
-int write_file_bin(const void *ptr, size_t size, size_t nmemb, char filename[]){
-    FILE *savefile;
-    savefile = fopen(filename, "wb");
+int write_file_bin(const void *ptr, size_t size, size_t nmemb, char filename[]){ //ptr è puntatore a cosa scrivere sul file, size è la dimensione di ogni elemento, dato che è
+    FILE *savefile; //un array di int la chiameremo con sizeof(int), nmemb è il numero di elementi da scrivere, e il filename è il nome del file. qui creiamo un puntatore a file
+    savefile = fopen(filename, "wb");   //apriamo il file in Write Binary mode
 
-    if (savefile == NULL){
+    if (savefile == NULL){  //controlliamo che non sia  inesistente il file
         fprintf(stderr, "cant open es1 for writing\n");
         exit(1);
 
     }
 
-    fwrite(ptr, size, nmemb, savefile);
-    fclose(savefile);
+    fwrite(ptr, size, nmemb, savefile); //chiamiamo fwrite con gli argomenti sopracitati
+    fclose(savefile);   //chiudiamo il file
 
 
 }
